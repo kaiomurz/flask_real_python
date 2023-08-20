36a22ba966ac7f218ee2080930d26a5f9be21e64
kaiomurz <kaiomurz@gmail.com>
2023-08-20 12:00:52 +0100

    interim commit with get and single person post complete

---
+++ people.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from flask import abort
+
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+PEOPLE = {
+    "Fairy": {
+        "fname": "Tooth",
+        "lname": "Fairy",
+        "timestamp": get_timestamp(),
+    },
+    "Ruprecht": {
+        "fname": "Knecht",
+        "lname": "Ruprecht",
+        "timestamp": get_timestamp(),
+    },
+    "Bunny": {
+        "fname": "Easter",
+        "lname": "Bunny",
+        "timestamp": get_timestamp(),
+    }
+}
+
+def read_all():
+    return list(PEOPLE.values())
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname", "")
+    
+    if lname and lname not in PEOPLE:
+        PEOPLE["lname"] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+
+        return PEOPLE, 201
+    else:
+        print(f"person {lname} already exists")
+        abort(
+            406,
+            f"Person with last name {lname} already exists",
+        )
+    
